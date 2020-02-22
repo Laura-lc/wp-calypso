523268d798fd653afaa85f75698141233012ba3a
Payton Swick <payton@foolord.com>
2020-02-22 13:12:48 -0500

    Use step title

--- packages/composite-checkout/demo/index.js
+++ packages/composite-checkout/demo/index.js
@@ -258,10 +258,7 @@ function AdditionalFields() {
 	);
 }
 
-const PaymentMethodStep = () => {
-	const { activeStepContent } = getDefaultPaymentMethodStep();
-	return <div>{ activeStepContent }</div>;
-};
+const paymentMethodStep = getDefaultPaymentMethodStep();
 
 const steps = [
 	getDefaultOrderSummaryStep(),
@@ -333,12 +330,11 @@ function MyCheckout() {
 			<Checkout>
 				<CheckoutSteps>
 					<CheckoutStep
-						title="Pick a payment method"
 						stepId="payment-method-step"
-						isComplete={ () => true }
-					>
-						<PaymentMethodStep />
-					</CheckoutStep>
+						isCompleteCallback={ () => true }
+						activeStepContent={ paymentMethodStep.activeStepContent }
+						titleContent={ paymentMethodStep.titleContent }
+					/>
 				</CheckoutSteps>
 			</Checkout>
 		</CheckoutProvider>
