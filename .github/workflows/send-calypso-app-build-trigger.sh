acc6bc967ffbf9ea313133de9240619651bfeafb
Noah Allen <noahtallen@gmail.com>
2020-02-25 20:38:08 -0800

    Upload archive file and metadata directly

--- .github/workflows/send-calypso-app-build-trigger.sh
+++ .github/workflows/send-calypso-app-build-trigger.sh
@@ -12,8 +12,14 @@ workflow_data="{
 	\"trigger_payload\": $trigger_payload
 }"
 
-curl -H "Content-Type: application/json" \
-	-d "$workflow_data" \
+# Create a zip of the FSE plugin. Should include built files at this point.
+build_archive=plugin-archive.zip
+zip -r $build_archive apps/full-site-editing/full-site-editing-plugin
+
+# Send metadata and build zip file to the endpoint.
+curl -v \
+	-F "meta=$workflow_data" \
+	-F "build_archive=@$build_archive;type=application/zip" \
 	"$TRIGGER_CALYPSO_APP_BUILD_ENDPOINT?calypso_app=$CALYPSO_APP"
 
 exit 0
